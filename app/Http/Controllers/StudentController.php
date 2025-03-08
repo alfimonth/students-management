c1e6efc24c54070da125b0b2e1314907b2c7cbd5
alfimonth <alvy1610@gmail.com>
2025-03-08 09:55:29 +0700

    Work on the  record create flow

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ClassesResource;
 use App\Http\Resources\StudentResource;
+use App\Models\Classes;
 use App\Models\Student;
 use Illuminate\Http\Request;
 
@@ -14,6 +16,15 @@ class StudentController extends Controller
 
         return inertia('Students/Index', [
             'students' => $students,
+        ]);
+    }
+
+    public function create()
+    {
+        $classes = ClassesResource::collection(Classes::all());
+
+        return inertia('Students/Create', [
+            'classes' => $classes,
         ]);
     }
 }
--- resources/js/Pages/Students/Index.vue
+++ resources/js/Pages/Students/Index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import MagnifyingGlass from '@/Components/Icons/MagnifyingGlass.vue';
 import Paginantion from '@/Components/Paginantion.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 
 defineProps({
     students: {
@@ -27,12 +27,12 @@ defineProps({
                     </div>
 
                     <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                        <a
-                            href="#"
+                        <Link
+                            :href="route('students.create')"
                             class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
                         >
                             Add Student
-                        </a>
+                        </Link>
                     </div>
                 </div>
 
